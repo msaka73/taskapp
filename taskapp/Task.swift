8024fd9dd027bafd9c3e55a690645d7d3159be95
msaka73 <msaka73@gmail.com>
2020-06-23 13:21:21 +0900

    task.swiftを追加。でもエラー発生

---
+++ taskapp/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 坂本充生 on 2020/06/23.
+//  Copyright © 2020 michio. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object{
+    
+    @objc dynamic var id = 0
+    
+}
